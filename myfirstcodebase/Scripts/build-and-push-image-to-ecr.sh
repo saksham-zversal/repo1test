55b72d53316ca41572a279e5ddc54703bb85423c
Saksham Paliwal <sakshampaliwal.zversal@gmail.com>
2024-03-19 17:49:17 +0530

    hi

--- myfirstcodebase/Scripts/build-and-push-image-to-ecr.sh
+++ myfirstcodebase/Scripts/build-and-push-image-to-ecr.sh
@@ -1,9 +1,17 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-docker build -t $imagename .
-docker tag $imagename:$IMAGE_TAG $Aws_account_id.dkr.ecr.$Region.amazonaws.com/$ECR_REPOSITORY:$IMAGE_TAG
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/base.env" set
 
-docker push $Aws_account_id.dkr.ecr.region.amazonaws.com/$ECR_REPOSITORY:$IMAGE_TAG
+aws ecr get-login-password --region $Region | docker login --username AWS --password-stdin $Aws_account_id.dkr.ecr.$Region.amazonaws.com
+
+aws ecr create-repository \
+    --repository-name $Repo_name_for_ecr \
+    --region $Region
+
+docker build -t "$imagename:$version" .
+docker tag "$imagename:$version" $Aws_account_id.dkr.ecr.$Region.amazonaws.com/$Repo_name_for_ecr
+
+docker push $Aws_account_id.dkr.ecr.region.amazonaws.com/$Repo_name_for_ecr
 
 # To push an image to the Amazon ECR repository, we need to authenticate the Docker client to the registry. 
 # Each registry requires its own authentication token. To proceed with the authentication, we would run a get-login-password command in 
